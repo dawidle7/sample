0fb4ddb1184d2b37700fcbc21ed87ce134060b88
dawidle7 <165098278+dawidle7@users.noreply.github.com>
2024-06-30 01:05:30 +0200

    Add files via upload

--- sample.sh
+++ sample.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 OPTSTR="he:"
+e=' -not -path "/dev/*" -a -not -path "/tmp/*" -a -not -path "/proc/*" -a -not -path "/sys/*" -a -not -path "/run/*" '
 while getopts ${OPTSTR} o
 do
     case $o in
-        e) echo "Excluding $OPTARG"; e=$OPTARG ;;
+        e) echo "Excluding $OPTARG"; e+=$OPTARG ;;
         :) echo "-$OPTARG requires an argument."; exit 1 ;;
         h | ?) echo "Usage sample.sh [-h] [-e DIR]"; exit 1 ;;
     esac
 done
-if [ -z "$e" ]; then e="(^\/proc|^\/sys|^\/dev)"; fi
+#e="(^\/proc|^\/sys|^\/dev)"; fi
+echo Final exclusion list $e
 
 
 echo Checking for gentoolkit...
@@ -22,9 +24,9 @@ then
         n) echo "Bye"; exit 1 ;;
         *) echo "Unrecognized selection: $ans"; exit 1 ;;
     esac
-
 else echo $emstatus
 fi
+
 
 echo Running as $LOGNAME
 if [ $LOGNAME == "root" ]
@@ -47,16 +49,15 @@ fi
 
 
 echo Looking for missing files which should be installed
-lif=""
+lif=()
 while read -r f
 do
-    lif+="$f
-"
+    lif+=("$f")
     # if ! [ -e "$f" ]
     # then
     #     pkg=$(equery b "$f")
     #     echo Missing file: $f from $pkg
-    #     read -t 2 -u 1 -p "Would you like to reinstall? y/n? " ans
+    #     read -t 1 -u 1 -p "Would you like to reinstall? y/n? " ans
     #     if ! [ -z $ans ]
     #     then
     #         case $ans in
@@ -68,15 +69,28 @@ do
     #     fi
     #     ans=""
     # fi
-done < <(equery f '*')
+done < <(equery f '*' )
+echo Size "${#lif[@]}"
+declare -A lifu
+for i in "${lif[@]}"; do lifu["$i"]=1; done
+lif=()
+for i in "${!lifu[@]}"; do lif+=("$i"); done
+unset $lifu
+echo New size "${#lif[@]}"
+
 
 
 echo Looking for files present which were not installed
 while read -r f
 do
-    if [[ "$f" =~ $e ]]; then continue; fi
-    if ! [[ $(echo "${lif[@]}" | grep -Fx $f) ]]
+    #if [[ "$f" =~ $e ]]; then continue; fi
+    if ! [[ $(echo "${lif[@]}" | grep -Fw "$f") ]]
     then
-        echo "value $f not found"
+        echo "$f not found"
+        echo "$f" >> ./sample.backup.files
+    else
+        echo "$f found"
     fi
-done < <(find / -iname '*' 2>./sample.err)
+done < <(eval 'find /etc '$e' 2>./sample.err')
+
+
